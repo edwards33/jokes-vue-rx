3e4e0e20830f7d965c0711abf1e67d66290c8aa0
Bo Persson <evalbahs@gmail.com>
2018-06-17 14:00:56 +0200

    ready to use

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,89 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section>
+    <b-loading :is-full-page="true" :active.sync="isLoadingFromHttp$" :can-cancel="true"></b-loading>
+    <b-table 
+      :data="dataFromApi$"
+      :paginated="true"
+      :per-page="5"
+      :current-page.sync="currentPage"
+      default-sort-direction="asc"
+      default-sort="id"
+      >
+      <template slot-scope="props">
+        <b-table-column field="id" label="ID" width="40" sortable numeric>
+          {{ props.row.id }}
+        </b-table-column>
+
+        <b-table-column field="joke" label="Joke" sortable>
+          {{replaceQuot(props.row.joke)}} 
+        </b-table-column>
+
+        <b-table-column label="Categories">
+          {{ getCategories(props.row.categories) }}
+        </b-table-column>
+      </template>
+      <template slot="footer">
+        <div class="columns">
+          <div class="column">
+          </div>
+          <div class="column has-text-centered">
+            <a class="button is-success" v-stream:click="clickOnLoad$">Load random jokes</a>
+          </div>
+          <div class="column">
+          </div>
+        </div>
+      </template>
+    </b-table>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {Observable} from 'rxjs/Rx'
+
+const JOKES_URL = 'http://api.icndb.com/jokes/random/15'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  domStreams: ["clickOnLoad$"],
+  data(){
+    return {
+      currentPage: 1
+    }
+  },
+  subscriptions(){
+    const createLoader = url => Observable.from(
+      this.$http.get(url)
+    ).pluck('data','value')
+
+    const dataFromApi$ = this.clickOnLoad$
+        .mapTo(JOKES_URL)
+        .exhaustMap(createLoader)
+        .catch(err => Observable.of([]))
+        .share()
+
+    const isLoadingFromHttp$ = Observable.merge(
+      this.clickOnLoad$.mapTo(true),
+      dataFromApi$.mapTo(false)
+    ).startWith(false)
+
+    return {
+      dataFromApi$,
+      isLoadingFromHttp$,
+    }
+  },
+  methods: {
+    getCategories(categories){
+      var categoriesString = ''
+      categories.forEach((item) => {
+        categoriesString += ' ' + item
+      })
+      return categoriesString
+    },
+    replaceQuot(text){
+      return text.replace(/&quot;/g, '\"')
+    }
   }
+
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Rx from 'rxjs'
+import VueRx from 'vue-rx'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
+
+Vue.use(Buefy)
+Vue.use(VueRx, Rx)
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
